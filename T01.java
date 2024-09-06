f3bd01e2adfdf91d807cd5bca4725be6950d8913
JOSUA GABRIEL PUTRA SIANTURI <sianturijosu@gmail.com>
2024-09-06 11:30:10 +0700

    finish

--- T01.java
+++ T01.java
@@ -4,31 +4,34 @@
 import java.util.*;
 import java.lang.Math;
 
+import java.util.*;
+import java.lang.Math;
+
 public class T01 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String iSBN;
-        String jUDUL;
-        String pENULIS;
-        int tAHUN;
-        String pENERBIT;
-        String fORMATBUKU;
-        double hARGA;
-        double mARGIN;
-        int sTOK;
-        double rATING;
+        String isbn;
+        String judul;
+        String penulis;
+        int tahun;
+        String penerbit;
+        String formatbuku;
+        double harga;
+        double margin;
+        int stok;
+        double rating;
 
-        iSBN = input.nextLine();
-        jUDUL = input.nextLine();
-        pENULIS = input.nextLine();
-        tAHUN = input.nextInt();
-        pENERBIT = input.nextLine();
-        fORMATBUKU = input.nextLine();
-        hARGA = input.nextDouble();
-        mARGIN = input.nextDouble();
-        sTOK = input.nextInt();
-        rATING = input.nextDouble();
-        System.out.println(iSBN + "|" + jUDUL + "|" + pENULIS + "|" + tAHUN + "|" + pENERBIT + "|" + fORMATBUKU + "|" + hARGA + "|" + mARGIN + "|" + sTOK + "|" + rATING);
+        isbn = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahun = input.nextInt();
+        penerbit = input.nextLine();
+        formatbuku = input.nextLine();
+        harga = input.nextDouble();
+        margin = input.nextDouble();
+        stok = input.nextInt();
+        rating = input.nextDouble();
+        System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahun + "|" + penerbit + "|" + formatbuku + "|" + harga + "|" + margin + "|" + stok + "|" + rating);
     }
 }
